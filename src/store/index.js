37a147e77cc9cf6049e2c84a8173268c1e06fdaf
igdkaur <igdkaur@gmail.com>
2022-01-18 07:30:49 +0530

    access counter from store

--- src/store/index.js
+++ src/store/index.js
@@ -1,18 +1,17 @@
 import { createStore } from 'redux'
 
-const counterReducer = (state = {count: 0}, action) => {
+const counterReducer = (state = {counter: 0}, action) => {
   if(action.type === 'increment')
   return {
-    count : state.count + 1
+    count : state.counter + 1
   }
   if(action.type === 'decrement')
   return {
-    count : state.count - 1
+    count : state.counter - 1
   }
   return state;
 }
 
 const store = createStore(counterReducer);
 
-store.dispatch({type: 'increment'})
-store.dispatch({type: 'decrement'})
+export default store;
